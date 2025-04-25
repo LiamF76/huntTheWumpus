8b3d458d49525696231b483ff60c85a23c8d3f3c
LiamF76 <lfogs76@gmail.com>
2025-04-25 13:34:48 -0400

    created graph
    created graph that will be the map used for the game

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 
 def main():
-    print("Hello, World!")
+    print("Hello, Wumpus!")
 
+    # graph cave map, rooms, then room numbers that room is connected to
+    cave_map = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        10: [],
+        11: [],
+        12: [],
+        13: [],
+        14: [],
+        15: [],
+        16: [],
+        17: [],
+        18: [],
+        19: [],
+        20: []
+
+
+    }
 
 
 
