6c7304e07ebd3e63c02eb31b2d4e04e81a230c97
LiamF76 <148378361+LiamF76@users.noreply.github.com>
2025-04-29 22:28:29 -0400

    added too much to add in msg

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
+## Liam Fogarty
+## DevOps
+## Final Project: Hunt the Wumpus
+
+import random
+
+# graph cave map, rooms, then room numbers that room is connected to
+cave_map = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
+    18: [],
+    19: [],
+    20: []
+}
+
+def game():
+    print("Game time!")
 
 def main():
     print("Hello, Wumpus!")
 
-    # graph cave map, rooms, then room numbers that room is connected to
-    cave_map = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
-        10: [],
-        11: [],
-        12: [],
-        13: [],
-        14: [],
-        15: [],
-        16: [],
-        17: [],
-        18: [],
-        19: [],
-        20: []
+    ## Define starting locations
+    player_location = random.randint(1, 20)
+    wumpus_location = random.randint(1, 20)
 
+    ## Prevent Wumpus and player from having same starting spot
+    while wumpus_location == player_location:
+        wumpus_location = random.randint(1, 20)
 
-    }
+    pit_locations = random.sample(range(1, 21), 2)  # generates randoms no duplicates, from chatGPT
+    while player_location in pit_locations or wumpus_location in pit_locations:
+        pit_locations = random.sample(range(1, 21), 2)
 
 
 
